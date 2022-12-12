c1fbd2e8ac0bbdc1de741468bf851a02218441b8
pavithrarajagopal <118541761+pavithrarajagopal@users.noreply.github.com>
2022-12-12 19:45:05 +0530

    Add files via upload

---
+++ week4/day1/testcases/leaftap/DuplicateLead.java
@@ -0,0 +1,76 @@
+package testcases.leaftap;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateLead {
+
+	public ChromeDriver driver;
+
+	@BeforeMethod
+	public void preCondition()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+	}
+
+	@Test
+	public void runDuplicateLead() throws InterruptedException 
+	{
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("//span[text()='Email']")).click();
+		driver.findElement(By.xpath("//input[@name='emailAddress']")).sendKeys("abcde123@gmail.com");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		Thread.sleep(3000);
+		//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		WebElement text = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-firstName']/a"));
+		String name = text.getText();
+		System.out.println(name);
+		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-firstName']/a")).click();
+		driver.findElement(By.linkText("Duplicate Lead")).click();
+		String title = driver.getTitle();
+		String actualTitle = "Duplicate Lead | opentaps CRM";
+		if(title.equalsIgnoreCase(actualTitle))
+		{
+			System.out.println("title matched");
+		}
+		else
+		{
+			System.out.println("title does not match");
+		}
+		//      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@name='submitButton']")).click();
+		WebElement text2 = driver.findElement(By.id("viewLead_firstName_sp"));
+		String duplicatename = text2.getText();
+		if(name.equals(duplicatename))
+		{
+			System.out.println("Confirm");
+		}
+	}
+
+	@AfterMethod
+	public void postCondition()
+	{
+		driver.close();
+	}
+	
+}
+
+
---
+++ week4/day1/testcases/leaftap/EditLead.java
@@ -0,0 +1,129 @@
+package testcases.leaftap;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLead {
+
+	public ChromeDriver driver;
+
+	@BeforeMethod
+	public void preCondition()
+	{
+		//* 1	Launch the browser
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.get("http://leaftaps.com/opentaps/control/main");
+
+		driver.manage().window().maximize();
+
+		//* 2	Enter the user name
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("Demosalesmanager");
+
+		//* 3	Enter the password
+
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+
+		//* 4	Click Login
+
+		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+	}
+
+	@Test
+	public void runEditLead() 
+	{
+		//* 5	Click crm/sfa link
+
+		driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+
+		//* 6	Click Leads link
+
+		driver.findElement(By.xpath("//a[(text()='Leads')]")).click();
+
+		//* 7	Click Find leads
+
+		driver.findElement(By.xpath("//a[(text()='Find Leads')]")).click();
+
+		//* 8	Enter first name
+
+		driver.findElement(By.xpath("(//input[@name='firstName'])[3]")).sendKeys("Pavithra");
+
+		driver.findElement(By.xpath("(//input[@name='lastName'])[3]")).sendKeys("Devi");
+
+
+		//* 9	Click Find leads button
+
+		driver.findElement(By.xpath("//button[contains(text(),'Find')]")).click();
+
+		//* 10 Click on first resulting lead
+
+		driver.findElement(By.xpath("//a[text()='15487']")).click();
+
+		//* 11 Verify title of the page
+
+		String title = driver.getTitle();
+		System.out.println("The title of the page is " +title);
+
+		if(title.contains("View Lead | opentaps CRM"))
+		{
+			System.out.println("I confirm the title");
+		}
+		else 
+		{
+			System.out.println("The title does not match");
+		}
+
+		//* 12 Click Edit
+
+		driver.findElement(By.xpath("//a[text()='Edit']")).click();
+
+		//* 13 Change the company name
+
+		driver.findElement(By.xpath("//input[@id='updateLeadForm_companyName']")).clear();
+
+
+		driver.findElement(By.xpath("//input[@id='updateLeadForm_companyName']")).sendKeys("changed to testleaf");
+
+		//* 14 Click Update
+
+		driver.findElement(By.xpath("//input[@value='Update']")).click();
+
+		//* 15 Confirm the changed name appears
+
+	}
+
+	@AfterMethod
+	public void postCondition()
+	{
+		driver.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
